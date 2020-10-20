0782b157b78e39c94a29ef906a78a82d00618086
Heath Lester <h.allen.lester@gmail.com>
2020-10-19 20:23:08 -0500

    added comments for fallback reference

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -1,26 +1,29 @@
 
+// Imports two functions from a source
 import { useConvictions, getConvictions } from "./ConvictionProvider.js"
 
+// Stores an HTML class in a variable
+const crimeFilterElement = document.querySelector(".filters__crime")
 
-const crimeFilterHTML = document.querySelector(".filters__crime")
-
-
+// 
 export const ConvictionSelect = () => {
-    getConvictions()
-    .then( () => {
-        const convictions = useConvictions()
-        render(convictions)   
-    })
+    debugger
+    // getConvictions()
+    // .then( () => {
+        const convictionsData = useConvictions()
+        crimesToHTML(convictionsData)   
+    // })
     
 }
 
-const render = convictionsCollection => {
 
-    crimeFilterHTML.innerHTML = `
+const crimesToHTML = convictionsArray => {
+    // 1) Tells HTML element that it is being written into.  2) Interpolates a section of HTML that creates an selection element dropdown menu.  3) Interpolates a .map() method that extracts the 'name' key from every object in an array.  4) Returns an interpolated and iterated setion of HTML from the data set.
+    crimeFilterElement.innerHTML = `
         <select class="dropdown" id="crimeSelect">
             <option value="0">Please select a crime...</option>
             ${
-                convictionsCollection.map(crime => {
+                convictionsArray.map(crime => {
                     const crimeName = crime.name
                     return `<option>${crimeName}</option>`
                 })
