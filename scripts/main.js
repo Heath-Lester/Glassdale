a0f1dee72aefefe0ff3bb0e295bd9db0de3e6fa1
Heath <h.allen.lester@gmail.com>
2020-10-22 15:15:27 -0500

    Revert "Filter for arresting officer"

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
 
 import {  criminalList  } from './criminals/CriminalList.js';
 import {  officerList  } from './officers/OfficerList.js';
-import {  ConvictionSelect  } from './convictions/ConvictionSelect.js';
-import { OfficerSelect } from './officers/OfficerSelect.js';
+import {  ConvictionSelect  } from './convictions/ConvictionSelect.js'
 
 criminalList()
 officerList()
 ConvictionSelect()
-OfficerSelect()
