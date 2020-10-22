35eb2f9b43004e266cedfa44887fb386f9605a84
Heath Lester <h.allen.lester@gmail.com>
2020-10-22 14:50:13 -0500

    Added OfficerSelect.js that renders Dropdown to initialise filtering.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 
 import {  criminalList  } from './criminals/CriminalList.js';
 import {  officerList  } from './officers/OfficerList.js';
-import {  ConvictionSelect  } from './convictions/ConvictionSelect.js'
+import {  ConvictionSelect  } from './convictions/ConvictionSelect.js';
+import { OfficerSelect } from './officers/OfficerSelect.js';
 
 criminalList()
 officerList()
 ConvictionSelect()
+OfficerSelect()
