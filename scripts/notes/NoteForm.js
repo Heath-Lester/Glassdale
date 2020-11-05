908d6209f7b2b9885f9e88d198ba53dd5462f7d5
Heath Lester <h.allen.lester@gmail.com>
2020-11-05 12:12:57 -0600

    Changed NoteForm and Note to interpolate Criminal Name from Id

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,18 +1,30 @@
-import{ saveNote } from './NoteProvider.js'
 
-const contentTarget = document.querySelector(".noteFormContainer")
+import { saveNote } from './NoteProvider.js'
+import { getCriminals, useCriminals } from '../criminals/CriminalProvider.js'
+
+
+const formElement = document.querySelector(".noteFormContainer")
 const eventHub = document.querySelector(".container")
 
 
-const render = () => {
-    contentTarget.innerHTML = `
-        <input type="date" id="note--date" name="date">
-        <input type="text" id="note--author" name="author">
-        <input type="text" id="note--suspect" name="suspect">
-        <textarea placeholder="Enter Note Here" id="note--content"></textarea>
-        <button id="saveNote">Save Note</button>
+const renderNoteForm = () => {
+    getCriminals()
+        .then(() => {
+            const criminalsArray = useCriminals()
+            formElement.innerHTML = `
+    <select id="noteForm--criminal" class="criminalSelect">
+        <option value="0">Please select a Criminal...</option>
+        ${criminalsArray.map(taco => {
+                return `<option value="${taco.id}">${taco.name}</option>`
+            }).join(" ")
+                }
+                <textarea placeholder="Enter Note Here..." id="note--text"></textarea>
+                <button id="saveNote">Save Note</button>
+    </select>
     `
+        })
 }
+
 
 // Handle browser-generated click event in component
 eventHub.addEventListener("click", clickEvent => {
@@ -20,19 +32,15 @@ eventHub.addEventListener("click", clickEvent => {
     // debugger
 
     if (clickEvent.target.id === "saveNote") {
-        
-        const date = document.querySelector("#note--date").value
-        const author = document.querySelector("#note--author").value
-        const suspect = document.querySelector("#note--suspect").value
-        const content = document.querySelector("#note--content").value
+
+        const text = document.querySelector("#note--text").value
+        const criminalId = document.querySelector(".criminalSelect").value
 
         // Make a new object representation of a note
         const newNote = {
             // Key/value pairs here
-            date,
-            author,
-            suspect,
-            content
+            text,
+            criminalId,
         }
 
         // Change API state and application state
@@ -42,5 +50,29 @@ eventHub.addEventListener("click", clickEvent => {
 
 
 export const NoteForm = () => {
-    render()
+    renderNoteForm()
 }
+
+
+// \/ \/ \/ OLD VERSION \/ \/ \/
+// const render = () => {
+//     contentTarget.innerHTML = `
+//         <input type="date" id="note--date" name="date">
+//         <input type="text" id="note--author" name="author">
+//         <input type="text" id="note--suspect" name="suspect">
+//         <textarea placeholder="Enter Note Here" id="note--content"></textarea>
+//         <button id="saveNote">Save Note</button>
+//     `
+// }
+
+// const date = document.querySelector("#note--date").value
+// const author = document.querySelector("#note--author").value
+// const suspect = document.querySelector("#note--suspect").value
+// const content = document.querySelector("#note--content").value
+
+// const newNote = {
+//     date,
+//     author,
+//     suspect,
+//     content
+// }
--- scripts/notes/noteList.js
+++ scripts/notes/noteList.js
@@ -1,37 +1,78 @@
 
+import { getCriminals, useCriminals } from "../criminals/CriminalProvider.js"
 import { getNotes, useNotes } from "./NoteProvider.js"
 import { notesToHTML } from "./Note.js"
+
 
 const eventHub = document.querySelector(".container")
 const notesElement = document.querySelector(".notesContainer")
 
+
+
 export const notesList = () => {
     // debugger
-    getNotes()
+    getCriminals()
+        .then(() => getNotes())
         .then(() => {
             let noteHtmlList = ``
             const notes = useNotes()
-            for (const note of notes) {
-                noteHtmlList += notesToHTML(note)
+            const criminalsArray = useCriminals()
+            // debugger
+                for (const note of notes) {
+                    let criminal = criminalsArray.find(criminal => { return criminal.id === parseInt(note.criminalId)})
+                    noteHtmlList += notesToHTML(note, criminal)
+                }
+                render(noteHtmlList)
             }
-            render(noteHtmlList)
-        })
+        )
 
 }
 
 
+
 eventHub.addEventListener("noteStateChanged", event => {
     console.log("Render Listener Pinged")
-    // debugger
-    const updatedNotes = useNotes()
+    debugger
     let noteHtmlList = ``
+    const updatedNotes = useNotes()
+    const criminalsArray = useCriminals()
     for (const note of updatedNotes) {
-        noteHtmlList += notesToHTML(note)
+        let criminal = criminalsArray.find(criminal => { return criminal.id === parseInt(note.criminalId)})
+        noteHtmlList += notesToHTML(note, criminal)
         render(noteHtmlList)
     }
 })
 
 
 const render = string => {
-    notesElement.innerHTML = string
+    notesElement.innerHTML = `<label class="formLabel">Notes</label>${string}`
 }
+
+
+// \/ \/ \/ OLD VERSION \/ \/ \/
+
+// eventHub.addEventListener("noteStateChanged", event => {
+//     console.log("Render Listener Pinged")
+//     // debugger
+//     const updatedNotes = useNotes()
+//     let noteHtmlList = ``
+//     for (const note of updatedNotes) {
+//         noteHtmlList += notesToHTML(note)
+//         render(noteHtmlList)
+//     }
+// })
+
+
+// export const notesList = () => {
+//     // debugger
+//     getNotes()
+//         .then(() => {
+//             let noteHtmlList = ``
+//             const notes = useNotes()
+//             for (const note of notes) {
+//                 noteHtmlList += notesToHTML(note)
+//             }
+//             render(noteHtmlList)
+//         })
+
+// }
