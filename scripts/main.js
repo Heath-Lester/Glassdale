e8b5cbfd7eda1774da069e7002d8cdbe5a6c6127
Heath Lester <h.allen.lester@gmail.com>
2020-11-09 15:05:31 -0600

    added delete note components to note modules

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 
-import { criminalList } from './criminals/CriminalList.js';
-import { officerList } from './officers/OfficerList.js';
 import { ConvictionSelect } from './convictions/ConvictionSelect.js';
 import { OfficerSelect } from './officers/OfficerSelect.js';
-import { NoteForm } from './notes/NoteForm.js';
+import { criminalList } from './criminals/CriminalList.js';
+import { officerList } from './officers/OfficerList.js';
 import { notesList } from  './notes/noteList.js';
-import "./criminals/AlibisList.js";
+import { NoteForm } from './notes/NoteForm.js';
 import "./witnesses/witnessButton.js";
 import "./witnesses/witnessList.js";
+import "./criminals/AlibisList.js";
 
 
 criminalList()
--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,9 +1,10 @@
 
 export const notesToHTML = (note, criminal) => {
     return `
-    <div class="note" id="note--${note.id}: ${note.criminalId}">
+    <div class="note" id="note--${note.id}:${note.criminalId}">
         <h4>Note on ${criminal.name}:</h4>
         <dt class"note__text">${note.text}</dt>
+        <button id="deleteNote--${note.id}">Delete</button>
     </div>
     `
 }
